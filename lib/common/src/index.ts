3661cb463de029f07cdd57b1a5ef6367c056306d
Sirwan <sir1afifi@gmail.com>
2020-12-26 18:32:11 +0330

    add query

--- lib/common/src/index.ts
+++ lib/common/src/index.ts
@@ -10,6 +10,22 @@ const startServer = async () => {
     console.log(
       $(".row .coffee-cups__wrapper .col-12 .smart-toc__anchor").length
     );
+    /* 
+      let result = [];
+      $(".row .coffee-cups__wrapper").each(function() {
+          const category = $(this).find("h2").text();
+          const items = $(this).nextUntil("img").each(function() {
+              const selector = '.wp-block-group__inner-container';
+              const selector2 = '.wp-block-columns .wp-block-column';
+              result.push({
+                  name: $(this).find(`${selector} ${selector2} h4`).text().trim(),
+                  description: $(this).find(`${selector} ${selector2} p`).text().trim(),
+                  recipe: $(this).find("> div > p:last").text().trim(),
+                  category 
+              });
+          });
+      })
+    */
   } catch (error) {
     console.log(`Error in fetching data`, error);
   }
